ca1de5a97b9f181a82efbf8e4ca0eaa5f8345313
Sofia <sofiarodrigues45@gmail.com>
2022-01-06 14:50:00 -0300

    adicionando validações

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,9 +8,20 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body;
 
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    if (categoryAlreadyExists) {
+        return res.status(400).json({ error: "A categoria já existe" });
+    }
+
     categoriesRepository.create({ name, description });
 
     return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req, res) => {
+    const all = categoriesRepository.list();
+
+    return res.json(all);
 });
 
 export { categoriesRoutes };
