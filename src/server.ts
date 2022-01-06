6490d56ccf74c346991d9d0182c521816fcb4569
Sofia <sofiarodrigues45@gmail.com>
2022-01-06 10:34:04 -0300

    criando rotas de categoria

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json("hello")
-})
+app.use(express.json());
 
-app.listen(3333, () => console.log("Servidor rodando"))
+app.use("/categories", categoriesRoutes)
+
+app.listen(3333, () => console.log("Servidor rodando"));
