79d3f0ea4ac5c53bafcae05637b8710eca725d28
Sofia <sofiarodrigues45@gmail.com>
2022-01-07 18:18:22 -0300

    adicionando rotas de especificações

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,26 @@
-class CreateSpepcificationService {
-    execute() {
-        console.log("todo");
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService {
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Especificação já existe!");
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
     }
 }
 
-export { CreateSpepcificationService };
+export { CreateSpecificationService };
