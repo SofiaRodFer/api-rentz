730e6cc0a65dfab017424d4a0e65681aacea841d
Sofia <sofiarodrigues45@gmail.com>
2022-01-19 10:29:32 -0300

    adicionando separações

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,12 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { CreateCategoryService } from "../modules/cars/useCases/createCategory/CreateCategoryUseCase";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
-    const { name, description } = req.body;
-
-    const createCategoryService = new CreateCategoryService(
-        categoriesRepository
-    );
-
-    createCategoryService.execute({ name, description });
-
-    return res.status(201).send();
 });
 
 categoriesRoutes.get("/", (req, res) => {
