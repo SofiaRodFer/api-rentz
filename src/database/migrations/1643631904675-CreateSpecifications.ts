bb3a09c3f88f832fe623894a975514fd1de8d7c7
Sofia <sofiarodrigues45@gmail.com>
2022-02-01 08:11:07 -0300

    refatorando em volta do banco de dados

--- src/database/migrations/1643631904675-CreateSpecifications.ts
+++ src/database/migrations/1643631904675-CreateSpecifications.ts
@@ -4,7 +4,7 @@ export class CreateSpecifications1643631904675 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "Specifications",
+                name: "specifications",
                 columns: [
                     {
                         name: "id",
@@ -30,6 +30,6 @@ export class CreateSpecifications1643631904675 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("Specifications");
+        await queryRunner.dropTable("specifications");
     }
 }
