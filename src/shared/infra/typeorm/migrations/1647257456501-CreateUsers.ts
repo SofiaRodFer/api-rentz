f310e835fe58c78cf44c6d7fec68630bbf751093
Sofia <sofiarodrigues45@gmail.com>
2022-04-28 09:33:23 -0300

    criando migrations de aluguel

--- src/shared/infra/typeorm/migrations/1647257456501-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1647257456501-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1647257456501 implements MigrationInterface {
                     {
                         name: "id",
                         type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
