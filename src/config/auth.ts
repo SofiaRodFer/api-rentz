9f40c42a6b8e9c8845235bd057c6a6a648510621
Sofia <sofiarodrigues45@gmail.com>
2022-08-12 12:09:49 -0300

    implementando refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "2fbc97ddc1fc820019dcc0cf2ddafe0a",
+    expires_in_token: "15m",
+    secret_refresh_token: "8c5c0bda9163f54c733dbb2445993f8c",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
